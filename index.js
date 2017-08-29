4f3748a61a8a1e08523d5effcff95de7bd3d3037
Michael Huensch <setup@run00.com>
2017-08-29 00:31:21 -0500

    Revert "Undoing static resource config for prod build.  Adding new config"
    This reverts commit 69c38529d46053885214d16e819043533e4e2410.

---
+++ index.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './src/components/App'
+import router from './src/router'
+import store from './src/store'
+
+Vue.config.productionTip = false
+
+import VeeValidate from 'vee-validate'
+Vue.use(VeeValidate)
+
+import 'vue-awesome/icons/times'
+import 'vue-awesome/icons/arrow-circle-right'
+import 'vue-awesome/icons/arrow-circle-left'
+import Icon from 'vue-awesome/components/Icon'
+Vue.component('icon', Icon)
+
+require('./node_modules/reset-css/reset.css')
+require('./node_modules/spectre.css/dist/spectre.css')
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+})
